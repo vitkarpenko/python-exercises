d09966a22fe58d590d906459f4bce0d5b6311c3f
Vitaly Karpenko <vitkarpenko@gmail.com>
2016-12-24 18:42:02 +0300

    13 exercise

--- 13.py
+++ 13.py
@@ -4,12 +4,14 @@ def fibonacci_sequence(n):
         yield a
         a, b = b, a + b
 
+
 def main():
     print('How much Fibonacci numbers do you want?')
     n = int(input('>>> '))
     for fib_num in fibonacci_sequence(n):
         print(fib_num, end=' ')
     print()
+
 
 if __name__ == '__main__':
     main()
