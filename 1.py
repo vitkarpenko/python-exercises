0fbc0f5f96174ee446f1a661a284d5d3427825eb
Vitaly Karpenko <vitkarpenko@gmail.com>
2016-12-24 18:34:38 +0300

    16 exercise

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
+def main():
+    name = input('Please enter your name: ')
+    age = int(input('Please enter your age: '))
+    times = int(input('How many times do you want me to print the message: '))
 
-name = input('Please enter your name: ')
-age = int(input('Please enter your age: '))
-times = int(input('How many times do you want me to print the message: '))
+    message = '\n{}, you will turn 100 years old in {}'.format(name, datetime.now().year - age + 100)
 
-message = '\n{}, you will turn 100 years old in {}'.format(name, datetime.now().year - age + 100)
+    print(message * times)
 
-print(message * times)
+if __name__ == '__main__':
+    main()
